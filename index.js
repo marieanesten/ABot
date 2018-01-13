9ce435c4908092a69bb79fae7dea718941505a44
Adam H <adam.email@comcast.net>
2018-01-12 20:32:51 -0500

    loads token easy w

--- index.js
+++ index.js
@@ -6,26 +6,29 @@
 */
 
 // Import the discord.js module
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+const fs = require("fs");
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'your bot token here';
+const token = fs.readFileSync("token.txt", {
+  encoding: "utf8"
+}).trim();
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
-client.on('ready', () => {
-  console.log('I am ready!');
+client.on("ready", () => {
+  console.log("I am ready!");
 });
 
 // Create an event listener for messages
-client.on('message', message => {
+client.on("message", message => {
   // If the message is "ping"
-  if (message.content === 'ping') {
+  if (message.content === "ping") {
     // Send "pong" to the same channel
-    message.channel.send('pong');
+    message.channel.send("pong");
   }
 });
 
